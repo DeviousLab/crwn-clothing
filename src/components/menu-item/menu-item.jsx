bbfb978748702177e5911a78a6dfe8916e2ac1b8
DeviousLab <deviouslab@gmail.com>
2021-09-14 08:58:37 +0400

    Styled homepage components
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './menu-item.scss'
 
 const MenuItem = ({ title , imageUrl, size }) => (
-    <div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}>
+        <div style={{backgroundImage: `url(${imageUrl})`}} className='background-image'/>
         <div className='content'>
             <div className='title'>{title}</div>
             <span className='subtitle'>SHOP NOW</span>
