1842dc805571750ee88312b457bcca477639f904
DeviousLab <deviouslab@gmail.com>
2021-09-15 20:24:47 +0400

    Initialised shop page
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import './App.css';
 import { Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop';
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/hats' component={HatsPage} />
+      <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
