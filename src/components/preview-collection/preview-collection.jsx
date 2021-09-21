8386342f613097ec4ef0d14900435fee064eb3a1
DeviousLab <deviouslab@gmail.com>
2021-09-21 17:08:57 +0400

    Created collection item component
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/components/preview-collection/preview-collection.jsx
+++ src/components/preview-collection/preview-collection.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item'
 
 import './preview-collection.scss'
 
@@ -7,7 +8,7 @@ const CollectionPreview = ({title, items}) => (
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
         {
-            items.filter((item, index) => index < 4).map(item => (<div key={item.id}>{item.name}</div>))
+            items.filter((item, index) => index < 4).map(({id, ...otherItemProps}) => (<CollectionItem key={id} {...otherItemProps} />))
         }
         </div>
     </div>
