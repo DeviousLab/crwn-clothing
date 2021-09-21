44005bbb9eb949e53e6884ff601c0d3799386ae6
DeviousLab <deviouslab@gmail.com>
2021-09-21 17:22:03 +0400

    Created header component
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header />
+      <Switch>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
