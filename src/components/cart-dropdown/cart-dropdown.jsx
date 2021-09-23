2592c838e9972cce3907c00b6026bda86dc8a3db
DeviousLab <deviouslab@gmail.com>
2021-09-23 15:19:04 +0400

    Fixed items overflowing in cart dropdown
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -7,14 +7,15 @@ import CustomButton from "../custom-button/custom-button";
 import './cart-dropdown.scss'
 
 const CartDropdown = ({ cartItems }) => (
-    <div className="cart-dropdown">
-        <div className="cart-items"/>
-        {
-            cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem} />))
-        }
+    <div className='cart-dropdown'>
+        <div className='cart-items'>
+            {cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
-)
+);
 
 const mapStateToProps = ({ cart: { cartItems } }) => ({
     cartItems
