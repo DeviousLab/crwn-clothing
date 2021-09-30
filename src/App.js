3a09c4d72eaeee517e835871ecb5629c12bf2064
DeviousLab <deviouslab@gmail.com>
2021-09-30 15:53:29 +0400

    Created checkout page
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
 import SignInAndRegister from './pages/sign-in-and-register/sign-in-and-register';
+import CheckoutPage from './pages/checkout/checkout';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
@@ -44,6 +46,7 @@ class App extends React.Component {
         <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} /> 
+        <Route exact path='/checkout' component={CheckoutPage} /> 
         <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndRegister />)} /> 
         </Switch>
       </div>
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -12,8 +12,12 @@ export const selectCartHidden = createSelector(
     (cart) => cart.hidden
 );
 
-
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     (cartItems) => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
-)
+);
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    (cartItems) => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0)
+);
