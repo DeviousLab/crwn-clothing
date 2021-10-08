5a3463e6ed4bf164ff3da6377b0a37b9a7c0c653
DeviousLab <deviouslab@gmail.com>
2021-10-08 23:00:35 +0400

    Integrated Stripe API into checkout
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.scss'
@@ -33,6 +35,12 @@ const CheckoutPage = ({ cartItems, cartTotal}) => (
         }
         <div className="total">
             <span>TOTAL: ${cartTotal}</span>
+        </div>
+        <StripeCheckoutButton price={cartTotal} />
+        <div className="test-warning">
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 10/22 - CVV: 123
         </div>
     </div>
 )
