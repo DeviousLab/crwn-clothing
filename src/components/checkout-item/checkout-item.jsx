429e244187bdb3e5e6e8d3d3ec976d81e02f3c6f
DeviousLab <deviouslab@gmail.com>
2021-10-28 00:16:20 +0400

    Revert "Component changes from main"
    This reverts commit 31a0ef96b68414703d7206a61314bce620382cac.

--- src/components/checkout-item/checkout-item.jsx
+++ src/components/checkout-item/checkout-item.jsx
@@ -1,45 +1,34 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import {
-  clearItem,
-  addItem,
-  removeItem
-} from '../../redux/cart/cart.actions';
+import { clearItem, addItem, removeItem } from "../../redux/cart/cart.actions";
 
+import './checkout-item.scss'
 
-const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
-  const { name, imageUrl, price, quantity } = cartItem;
-  return (
-    <div className='checkout-item'>
-      <div className='image-container'>
-        <img src={imageUrl} alt='item' />
-      </div>
-      <span className='name'>{name}</span>
-      <span className='quantity'>
-        <div className='arrow' onClick={() => removeItem(cartItem)}>
-          &#10094;
+const CheckoutItem = ({ cartItem, removeItem, addItem, clearItem }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+    return (
+        <div className="checkout-item">
+            <div className="image-container">
+                <img src={imageUrl} alt="product" />
+            </div>
+            <span className="name">{name}</span>
+            <div className="quantity">
+                <button className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</button>
+                <span className="value">{quantity}</span>
+                <button className="arrow" onClick={() => addItem(cartItem)}>&#10095;</button>
+            </div>
+            <span className="price">${price}</span>
+            <button className="remove-button" onClick={() => clearItem(cartItem)}>
+                <span>&#10005;</span>
+            </button>
         </div>
-        <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={() => addItem(cartItem)}>
-          &#10095;
-        </div>
-      </span>
-      <span className='price'>{price}</span>
-      <div className='remove-button' onClick={() => clearItem(cartItem)}>
-        &#10005;
-      </div>
-    </div>
-  );
-};
+)};
 
 const mapDispatchToProps = dispatch => ({
-  clearItem: item => dispatch(clearItem(item)),
-  addItem: item => dispatch(addItem(item)),
-  removeItem: item => dispatch(removeItem(item))
-});
+    clearItem: item => dispatch(clearItem(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
+})
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(CheckoutItem);
+export default connect(null, mapDispatchToProps)(CheckoutItem)
--- src/components/collections-overview/collections-overview.jsx
+++ src/components/collections-overview/collections-overview.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../preview-collection/preview-collection'
+import CollectionPreview from '../preview-collection/preview-collection';
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
 
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
-
+import './collections-overview.scss';
 
 const CollectionsOverview = ({ collections }) => (
-  <div className='collections-overview'>
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
-  </div>
+    <div className="collections-overview">
+        {
+            collections.map(({ id, ...otherCollectionProps }) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
+        }
+    </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
+    collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
