f92b3b8cab96766734c08f688c6f6dec21baada4
DeviousLab <deviouslab@gmail.com>
2021-10-28 00:14:24 +0400

    Update pages from main
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './homepage.scss';
 
 import Directory from '../../components/directory/directory';
 
-const Homepage = () => (
-    <div className="homepage">
-        <Directory />
-    </div>
-    );
 
-export default Homepage;
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
