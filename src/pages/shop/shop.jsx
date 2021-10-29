f03c8881736956fdb803a93d721c08436f2d5778
DeviousLab <deviouslab@gmail.com>
2021-10-30 02:25:27 +0400

    Added new selector for loading spinner
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -8,7 +8,7 @@ import CollectionPage from "../collection/collection";
 import WithSpinner from "../../components/with-spinner/with-spinner";
 
 import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
-import { selectIsCollectionFetching } from "../../redux/shop/shop.selector";
+import { selectIsCollectionFetching, selectIsCollectionsLoaded } from "../../redux/shop/shop.selector";
 
 const CollectionsOverviewWithSpinner = WithSpinner(collectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
@@ -19,18 +19,19 @@ class ShopPage extends React.Component {
         fetchCollectionsStartAsync();
     }
     render() {
-        const { match, isCollectionFetching } = this.props;
+        const { match, isCollectionsLoaded } = this.props;
         return (
             <div className="shop-page">
-                <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props}/>} />
-                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props}/>} />
+                <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={!isCollectionsLoaded} {...props}/>} />
+                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props}/>} />
             </div>
         )
     }
 };
 
 const mapStateToProps = createStructuredSelector({
-    isCollectionFetching: selectIsCollectionFetching
+    isCollectionFetching: selectIsCollectionFetching,
+    isCollectionsLoaded: selectIsCollectionsLoaded
 });
 
 const mapDispatchToProps = dispatch => ({
