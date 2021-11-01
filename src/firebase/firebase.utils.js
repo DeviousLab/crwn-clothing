4df2942750bce039e86257edd5c5a5e7be60f1ef
DeviousLab <deviouslab@gmail.com>
2021-11-01 06:04:30 +0400

    Refactored sign up into sagas
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,8 +11,6 @@ const firebaseConfig = {
     appId: "1:220996651185:web:a326737d41fa27f2317a90"
 };
 
-firebase.initializeApp(firebaseConfig);
-
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
 
@@ -60,6 +58,8 @@ export const convertCollectionsSnapshotToMap = (collections) => {
         return accumulator;
     }, {});
 }
+
+firebase.initializeApp(firebaseConfig);
 
 export const getCurrentUser = () => {
     return new Promise((resolve, reject) => {
