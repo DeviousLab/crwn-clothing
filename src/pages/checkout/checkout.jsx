c929bf91d1fe42172e09b5b0b82bd338812bc9e9
DeviousLab <deviouslab@gmail.com>
2021-11-16 20:16:57 +0400

    Converted other pages to use react hooks
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { useSelector } from 'react-redux';
+
 
 import CheckoutItem from '../../components/checkout-item/checkout-item';
 import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
@@ -9,45 +9,45 @@ import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector
 
 import './checkout.scss'
 
-const CheckoutPage = ({ cartItems, cartTotal}) => (
-    <div className="checkout-page">
-        <div className="checkout-header">
-            <div className="header-block">
-                <span>Product</span>
-            </div>
-            <div className="header-block">
-                <span>Description</span>
-            </div>
-            <div className="header-block">
-                <span>Quantity</span>
-            </div>
-            <div className="header-block">
-                <span>Price</span>
-            </div>
-            <div className="header-block">
-                <span>Remove</span>
-            </div>
-        </div>
-        {
-            cartItems.map(cartItem => (
-                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-            ))
-        }
-        <div className="total">
-            <span>TOTAL: ${cartTotal}</span>
-        </div>
-        <StripeCheckoutButton price={cartTotal} />
-        <div className="test-warning">
-            *Please use the following test credit card for payments*
-            <br />
-            4242 4242 4242 4242 - Exp: 10/22 - CVV: 123
-        </div>
-    </div>
-)
+const CheckoutPage = () => {
+    const cartItems = useSelector(selectCartItems);
+    const total = useSelector(selectCartTotal);
 
-const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItems,
-    cartTotal: selectCartTotal
-});
+    return (
+        <div className="checkout-page">
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
+            </div>
+            {
+                cartItems.map(cartItem => (
+                    <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+                ))
+            }
+            <div className="total">
+                <span>TOTAL: ${total}</span>
+            </div>
+            <StripeCheckoutButton price={total} />
+            <div className="test-warning">
+                *Please use the following test credit card for payments*
+                <br />
+                4242 4242 4242 4242 - Exp: 10/22 - CVV: 123
+            </div>
+        </div>
+    )
+}
 
-export default connect(mapStateToProps)(CheckoutPage);
+export default CheckoutPage;
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
 import CollectionPageContainer from "../collection/collection.container";
 
-import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
-
 const ShopPage = ({ fetchCollectionsStart, match }) => {
+    const dispatch = useDispatch();
+    const fetchCollectionsStartHandler = dispatch(fetchCollectionsStart());
+
     useEffect(() => {
-        fetchCollectionsStart();
-    }, [fetchCollectionsStart]);
+        fetchCollectionsStartHandler();
+    }, [fetchCollectionsStartHandler]);
+
     return (
         <div className="shop-page">
             <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
@@ -19,9 +21,4 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
     )
 };
 
-
-const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
-});
-
-export default connect(null, mapDispatchToProps)(ShopPage);
+export default ShopPage;
