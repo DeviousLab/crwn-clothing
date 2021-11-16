7f303473b5e86ec56557ea49316421eab093857c
DeviousLab <deviouslab@gmail.com>
2021-11-16 20:24:10 +0400

    Reverted back to react redux for shop page
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,18 +1,16 @@
 import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { connect } from "react-redux";
 
 import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
 import CollectionPageContainer from "../collection/collection.container";
 
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
+
 const ShopPage = ({ fetchCollectionsStart, match }) => {
-    const dispatch = useDispatch();
-    const fetchCollectionsStartHandler = dispatch(fetchCollectionsStart());
-
     useEffect(() => {
-        fetchCollectionsStartHandler();
-    }, [fetchCollectionsStartHandler]);
-
+        fetchCollectionsStart();
+    }, [fetchCollectionsStart]);
     return (
         <div className="shop-page">
             <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
@@ -21,4 +19,9 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
     )
 };
 
-export default ShopPage;
+
+const mapDispatchToProps = dispatch => ({
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
