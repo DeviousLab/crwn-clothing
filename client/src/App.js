46261f373764afd33719f9941146c6690a586955
DeviousLab <deviouslab@gmail.com>
2021-11-25 23:53:09 +0400

    Implemented react lazy and suspense
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import HomePage from './pages/homepage/homepage';
-import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
-import SignInAndRegister from './pages/sign-in-and-register/sign-in-and-register';
-import CheckoutPage from './pages/checkout/checkout';
 
 import { GlobalStyle } from './global.styles';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage'));
+const ShopPage = lazy(() => import('./pages/shop/shop'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout'));
+const SignInAndRegister = lazy(() => import('./pages/sign-in-and-register/sign-in-and-register'));
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -32,10 +33,12 @@ class App extends React.Component {
         <GlobalStyle />
         <Header />
         <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} /> 
-        <Route exact path='/checkout' component={CheckoutPage} /> 
-        <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndRegister />)} /> 
+          <Suspense fallback={<div>...Loading</div>}>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} /> 
+            <Route exact path='/checkout' component={CheckoutPage} /> 
+            <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndRegister />)} /> 
+          </Suspense>
         </Switch>
       </div>
     );
